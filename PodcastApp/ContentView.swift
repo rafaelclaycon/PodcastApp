388ab76c63a4de9a43fd71468327005b4d127e13
Rafael Claycon Schmitt <rafael.claycon@gmail.com>
2020-11-25 08:55:08 -0300

    Initial commit

---
+++ PodcastApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PodcastApp
+//
+//  Created by Rafael Schmitt on 25/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PodcastApp/PodcastAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastAppApp.swift
+//  PodcastApp
+//
+//  Created by Rafael Schmitt on 25/11/20.
+//
+
+import SwiftUI
+
+@main
+struct PodcastAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
