d87a7971dc5eac6b366981f482b72ac6c82b3b1f
Rafael Claycon Schmitt <rafael.claycon@gmail.com>
2020-11-29 12:31:23 -0300

    Add search bar to Discover tab

---
+++ PodcastApp/Extensions/UIApplication+Extensions.swift
@@ -0,0 +1,32 @@
+//
+//  UIApplication+Extensions.swift
+//  PodcastApp
+//
+//  Created by Rafael Schmitt on 29/11/20.
+//
+
+import SwiftUI
+
+extension UIApplication {
+    func endEditing(_ force: Bool) {
+        self.windows
+            .filter{$0.isKeyWindow}
+            .first?
+            .endEditing(force)
+    }
+}
+
+struct ResignKeyboardOnDragGesture: ViewModifier {
+    var gesture = DragGesture().onChanged{_ in
+        UIApplication.shared.endEditing(true)
+    }
+    func body(content: Content) -> some View {
+        content.gesture(gesture)
+    }
+}
+
+extension View {
+    func resignKeyboardOnDragGesture() -> some View {
+        return modifier(ResignKeyboardOnDragGesture())
+    }
+}
--- PodcastApp/Views/Discover/DiscoverView.swift
+++ PodcastApp/Views/Discover/DiscoverView.swift
@@ -8,22 +8,55 @@
 import SwiftUI
 
 struct DiscoverView: View {
+    let array = ["Peter", "Paul", "Mary", "Anna-Lena", "George", "John", "Greg", "Thomas", "Robert", "Bernie", "Mike", "Benno", "Hugo", "Miles", "Michael", "Mikel", "Tim", "Tom", "Lottie", "Lorrie", "Barbara"]
+    @State private var searchText = ""
+    @State private var showCancelButton: Bool = false
+    
     var body: some View {
         NavigationView {
             VStack {
-                ScrollView(.horizontal) {
-                    Text("Podcast A")
-                        .padding()
-                    
-                    Text("Podcast B")
-                        .padding()
-                    
-                    Text("Podcast C")
-                        .padding()
+                HStack {
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+
+                        TextField("Search", text: $searchText, onEditingChanged: { isEditing in
+                            self.showCancelButton = true
+                        }, onCommit: {
+                            print("onCommit")
+                        }).foregroundColor(.primary)
+
+                        Button(action: {
+                            self.searchText = ""
+                        }) {
+                            Image(systemName: "xmark.circle.fill").opacity(searchText == "" ? 0 : 1)
+                        }
+                    }
+                    .padding(EdgeInsets(top: 8, leading: 6, bottom: 8, trailing: 6))
+                    .foregroundColor(.secondary)
+                    .background(Color(.secondarySystemBackground))
+                    .cornerRadius(10.0)
+
+                    if showCancelButton  {
+                        Button("Cancel") {
+                            UIApplication.shared.endEditing(true) // This must be placed before the other commands here.
+                            self.searchText = ""
+                            self.showCancelButton = false
+                        }
+                        .foregroundColor(Color(.systemBlue))
+                    }
                 }
+                .padding(.horizontal)
+                .navigationBarHidden(showCancelButton)
                 
-                Spacer()
-            }.navigationBarTitle(Text("Discover"))
+                List {
+                    // Filtered list of names.
+                    ForEach(array.filter{$0.hasPrefix(searchText) || searchText == ""}, id:\.self) {
+                        searchText in Text(searchText)
+                    }
+                }
+                .navigationBarTitle(Text("Discover"))
+                .resignKeyboardOnDragGesture()
+            }
         }
     }
 }
